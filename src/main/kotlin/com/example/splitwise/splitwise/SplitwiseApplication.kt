a2469010da8fb3586148dedebb3efe8745fbe7c9
Amit Verma <amit.verma@hashedin.com>
2020-05-21 09:36:25 +0530

    start with the spring functionality

---
+++ src/main/kotlin/com/example/splitwise/splitwise/SplitwiseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.splitwise.splitwise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitwiseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SplitwiseApplication>(*args)
+}
