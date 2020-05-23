01157275aad345ac3d3b25a45bfa43563f81b125
Amit Verma <amit.verma@hashedin.com>
2020-05-23 14:05:00 +0530

    bill controller and service with repository

--- src/main/kotlin/com/example/splitwise/splitwise/controller/BillController.kt
+++ src/main/kotlin/com/example/splitwise/splitwise/controller/BillController.kt
@@ -1,35 +1,40 @@
 package com.example.splitwise.splitwise.controller
 
-//import com.example.splitwise.splitwise.dto.BillDto
-//import com.example.splitwise.splitwise.dto.ResponseDto
-//import com.example.splitwise.splitwise.service.BillServiceImpl
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.http.HttpStatus
-//import org.springframework.http.ResponseEntity
-//import org.springframework.web.bind.annotation.*
-//
-//@RestController
-//@RequestMapping("/bill")
-//class BillController {
+import com.example.splitwise.splitwise.dto.BillGenerateDto
+import com.example.splitwise.splitwise.dto.BillUpdateDto
+import com.example.splitwise.splitwise.dto.ResponseDto
+import com.example.splitwise.splitwise.service.BillServiceImpl
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
-//    @Autowired
-//    lateinit var billService: BillServiceImpl
-//
-//    @PostMapping("/")
-//    fun generateBill(@RequestBody billDto: BillDto): ResponseEntity<ResponseDto> {
-//        val generateBill = billService.generateBill(billDto)
-//        var response = ResponseDto("Successfully generated", generateBill, HttpStatus.CREATED)
-//        return ResponseEntity.status(HttpStatus.CREATED).body(response)
-//    }
-//
-//    @GetMapping("/{billId}")
-//    fun getBill(@PathVariable(value = "billId") billId: Long): ResponseEntity<ResponseDto> {
-//        val bill = billService.getBill(billId)
-//        if (bill == null) {
-//            var response = ResponseDto("Bill not present (might be bill id is wrong)", BillDto::class, HttpStatus.NOT_FOUND)
-//            return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
-//        }
-//        var response = ResponseDto("Successfully Found", bill, HttpStatus.FOUND)
-//        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
-//    }
-//}
+@RestController
+@RequestMapping("/bill")
+class BillController {
+
+    @Autowired
+    lateinit var billService: BillServiceImpl
+
+    @PostMapping("/")
+    fun generateBill(@RequestBody billGenerateDto: BillGenerateDto): ResponseEntity<ResponseDto> {
+        val generateBill = billService.generateBill(billGenerateDto)
+        var response = ResponseDto("Successfully generated", generateBill, HttpStatus.CREATED)
+        return ResponseEntity.status(HttpStatus.CREATED).body(response)
+    }
+
+    @GetMapping("/{billId}")
+    fun getBill(@PathVariable(value = "billId") billId: Long): ResponseEntity<ResponseDto> {
+        val bill = billService.getBill(billId)
+        var response = ResponseDto("Successfully fetched  bill ", bill, HttpStatus.NOT_FOUND)
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
+
+    }
+
+    @PutMapping("/")
+    fun updateBill(billUpdateDto: BillUpdateDto): ResponseEntity<ResponseDto> {
+        var bill = billService.updateBill(billUpdateDto = billUpdateDto)
+        var response = ResponseDto("Successfully update  bill ", bill, HttpStatus.NOT_FOUND)
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
+    }
+}
