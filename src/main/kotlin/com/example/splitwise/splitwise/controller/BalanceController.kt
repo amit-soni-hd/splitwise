d8258132cda50ad1074aa3fc4981f5857dbed01c
Amit Verma <amit.verma@hashedin.com>
2020-05-23 14:08:38 +0530

    create the balance controller and service with database

--- src/main/kotlin/com/example/splitwise/splitwise/controller/BalanceController.kt
+++ src/main/kotlin/com/example/splitwise/splitwise/controller/BalanceController.kt
@@ -1,33 +1,33 @@
 package com.example.splitwise.splitwise.controller
 
-//import com.example.splitwise.splitwise.dto.ResponseDto
-//import com.example.splitwise.splitwise.service.BalanceServiceImpl
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.http.HttpStatus
-//import org.springframework.http.ResponseEntity
-//import org.springframework.web.bind.annotation.*
-//
-//@RestController
-//@RequestMapping("/balance")
-//class BalanceController {
-//
-//    @Autowired
-//    private lateinit var balanceService: BalanceServiceImpl
-//
-//    @GetMapping("/total/{emailId}")
-//    fun getTotalBalance(@PathVariable("emailId") emailId:String): ResponseEntity<ResponseDto> {
-//        val totalBalance = balanceService.getTotalBalance(emailId)
-//        var response = ResponseDto("Total balance", totalBalance, HttpStatus.ACCEPTED)
-//        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
-//    }
-//
-//    @GetMapping("/individual/{emailId}/{respectId}")
-//    fun getIndividualBalance(@RequestParam("emailId") emailId: String, @RequestParam("respectId") respectId: String): ResponseEntity<ResponseDto> {
-//
-//        val individualBalance = balanceService.getIndividualBalance(emailId, respectId)
-//        var response = ResponseDto("Individual Balance", individualBalance, HttpStatus.ACCEPTED)
-//        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
-//    }
+import com.example.splitwise.splitwise.dto.ResponseDto
+import com.example.splitwise.splitwise.service.BalanceServiceImpl
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/balance")
+class BalanceController {
+
+    @Autowired
+    private lateinit var balanceService: BalanceServiceImpl
+
+    @GetMapping("/total/{userId}")
+    fun getTotalBalance(@PathVariable("userId") userId:Long): ResponseEntity<ResponseDto> {
+        val totalBalance = balanceService.getTotalBalance(userId = userId)
+        var response = ResponseDto("Total balance", totalBalance, HttpStatus.ACCEPTED)
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
+    }
+
+    @GetMapping("/individual/{userId}/{respectId}")
+    fun getIndividualBalance(@RequestParam("userId") userId: Long, @RequestParam("respectId") respectId: Long): ResponseEntity<ResponseDto> {
+
+        val individualBalance = balanceService.getIndividualBalance(userId = userId, respectUserId = respectId)
+        var response = ResponseDto("Individual Balance", individualBalance, HttpStatus.ACCEPTED)
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response)
+    }
 
 
-//}
+}
